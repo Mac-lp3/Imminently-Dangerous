30d85457d65b37c210700550d9c202222bc5ba44
Mac-lp3 <staticlp3@gmail.com>
2016-10-08 18:50:23 -0400

    version 1 of two field interpreter

--- js/inputParser.js
+++ js/inputParser.js
@@ -10,19 +10,65 @@ module.exports = (function() {
 	 */
 	const si = function splitInput(searchString) {
 
+		let isUsableInput = false;
+
+		const vals = searchString.split(' ');
+
+		if (vals.length == 1) {
+			// name/company/building
+
+		} else if (vals.length == 2) {
+
+			// name/company/building, or street + number (200 Wallnut)
+
+			let address;
+
+			if (Number.isInteger(vals[0])) {
+				
+				if (!Number.isInteger(vals[1])) {
+					isUsableInput = true;
+				} else {
+					// probs an address. should search for company name as well.
+					getAddressObject(searchString);
+				}
+
+			} else {
+
+				isUsableInput = true;
+
+				if (Number.isInteger(vals[1])) {
+					// could just be backwards
+					getAddressObject([vals[1], vals[2]]);
+				} else {
+					// probs a company name
+					getAddressObject([vals[0], vals[1]]);
+				}
+			}
+
+		} else if (vals.length == 3) {
+			// name/company/building, or street + number (200 Cherry St)
+
+		} else if (vals.length == 4) {
+			// name/company/building, or street + number (200 Mt Vernon St)
+
+		} else if (vals.length == 5) {
+			// name/company/building, or street + number + zip(200 Mt Vernon St 19190)
+
+		}
+
 	};
 
 	/*
 	 * 
 	 */
-	const ia = function isAddress(){
+	const isAddress = function isAddress(){
 
 	};
 
 	/*
 	 * Builds a address object from a given input string.
 	 */
-	const bao = function getAddressObject() {
+	const getAddressObject = function getAddressObject(obj) {
 
 	};
 
