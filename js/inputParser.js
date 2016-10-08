296a348b97eb3a18739b0b71dd3d68c48d4a302a
Mac-lp3 <staticlp3@gmail.com>
2016-10-08 18:28:10 -0400

    utility class for interpreting search strings

---
+++ js/inputParser.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// TODO given a string, i need to know if it is an address.
+// if it is, break it up into the parts.
+// if it is not, it could be a building/company/person name.
+module.exports = (function() {
+
+	/*
+	 * Splits the given search sting by spaces, returning an array.
+	 */
+	const si = function splitInput(searchString) {
+
+	};
+
+	/*
+	 * 
+	 */
+	const ia = function isAddress(){
+
+	};
+
+	/*
+	 * Builds a address object from a given input string.
+	 */
+	const bao = function getAddressObject() {
+
+	};
+
+	return {
+
+	};
+
+})();
